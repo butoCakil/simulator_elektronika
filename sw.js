733921806526420603321c3e83cf55389cf8aead
butoCakil <bennysurahman@gmail.com>
2026-07-31 19:31:20 +0700

    feat: tambah/upgrade 10 komponen

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    NAIKKAN nomor VERSI setiap kali index.html diubah, agar peramban tahu ada
    pembaruan dan simpanan lama dibersihkan.
    ========================================================================== */
-const VERSI = 'simbion-v2.1';
+const VERSI = 'simbion-v2.2';
 const BERKAS = [
   './',
   './index.html',
