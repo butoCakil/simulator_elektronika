2bc620a910bb99b7e0a2efbe1f09d292b852a1ea
butoCakil <bennysurahman@gmail.com>
2026-07-31 11:23:20 +0700

    edit: contoh op-amp inverting

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    NAIKKAN nomor VERSI setiap kali index.html diubah, agar peramban tahu ada
    pembaruan dan simpanan lama dibersihkan.
    ========================================================================== */
-const VERSI = 'sasmita-v1.7';
+const VERSI = 'sasmita-v1.8';
 const BERKAS = [
   './',
   './index.html',
